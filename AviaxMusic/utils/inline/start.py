7e14c0d6a12d68089a6da205055c7b4e232f1014
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2024-03-31 22:12:24 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -27,6 +27,8 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_5"], url=config.SUPPORT_GROUP),
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            
+            ),
             InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/tso_king"),
         ],
     ]
