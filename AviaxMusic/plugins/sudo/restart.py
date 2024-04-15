8e8112543233d23a115de0a1d9240e66d06b48ee
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2024-04-15 17:25:38 +0530

    Update restart.py

--- AviaxMusic/plugins/sudo/restart.py
+++ AviaxMusic/plugins/sudo/restart.py
@@ -27,7 +27,7 @@ async def is_heroku():
     return "heroku" in socket.getfqdn()
 
 
-@app.on_message(filters.command(["getlog", "logs", "getlogs"]) & SUDOERS)
+@app.on_message(filters.command(["getesstlog", "lojknbfgs", "getltrwaogs"]) & SUDOERS)
 @language
 async def log_(client, message, _):
     try:
