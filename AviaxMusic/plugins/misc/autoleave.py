4b296f93d91ba9f760c1eb4e0fa61814385e1e11
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2024-11-05 23:55:31 +0530

    Update autoleave.py

--- AviaxMusic/plugins/misc/autoleave.py
+++ AviaxMusic/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOG_GROUP_ID
-                                and i.chat.id != -1001686672798
-                                and i.chat.id != -1001549206010
+                                and i.chat.id != -1001995053703
+                                and i.chat.id != -1001995053703
                             ):
                                 if left == 20:
                                     continue
